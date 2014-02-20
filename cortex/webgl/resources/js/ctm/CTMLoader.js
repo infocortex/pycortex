ad88b54bd178a33faf7e77332d6cf57a48238f38
Chris Filo Gorgolewski <chris.gorgolewski@gmail.com>
2014-02-20 18:51:22 +0100

    fixed paths

--- cortex/webgl/resources/js/ctm/CTMLoader.js
+++ cortex/webgl/resources/js/ctm/CTMLoader.js
@@ -106,7 +106,7 @@ THREE.CTMLoader.prototype.load = function( url, callback, useWorker, useBuffers,
 
 				if ( useWorker ) {
 
-					var worker = new Worker( "resources/js/ctm/CTMWorker.js" );
+					var worker = new Worker( "/static/pycortex-resources/js/ctm/CTMWorker.js" );
 
 					worker.onmessage = function( event ) {
 
